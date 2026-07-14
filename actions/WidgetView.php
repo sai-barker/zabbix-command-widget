ea3bf227735fb505bf1cabb90629c28c1339420d
sai-barker <sai.barker95@gmail.com>
2026-07-14 21:55:59 +1000

    Discover available Zabbix scripts from widget

--- actions/WidgetView.php
+++ actions/WidgetView.php
@@ -2,14 +2,44 @@
 
 namespace Modules\ZabbixCommandWidget\Actions;
 
+use API;
 use CControllerDashboardWidgetView;
 use CControllerResponseData;
 
 class WidgetView extends CControllerDashboardWidgetView {
 
     protected function doAction(): void {
+
+        $hostid = $this->fields_values['hostid'][0] ?? null;
+
+        $hostname = '(None)';
+
+        if ($hostid !== null) {
+            $hosts = API::Host()->get([
+                'output' => ['host', 'name'],
+                'hostids' => [$hostid]
+            ]);
+
+            if ($hosts) {
+                $hostname = $hosts[0]['name'];
+            }
+        }
+
+        $scripts = API::Script()->get([
+            'output' => [
+                'scriptid',
+                'name',
+                'scope',
+                'type'
+            ],
+            'sortfield' => 'name'
+        ]);
+
         $this->setResponse(new CControllerResponseData([
             'name' => $this->getInput('name', $this->widget->getName()),
+            'hostid' => $hostid,
+            'hostname' => $hostname,
+            'scripts' => $scripts,
             'user' => [
                 'debug_mode' => $this->getDebugMode()
             ]
--- views/widget.view.php
+++ views/widget.view.php
@@ -5,8 +5,27 @@
  * @var array $data
  */
 
+$items = [
+    new CDiv('Hello World'),
+    new CTag('hr'),
+    new CDiv('Host ID: '.$data['hostid']),
+    new CDiv('Host Name: '.$data['hostname']),
+    new CTag('hr'),
+    new CTag('h4', true, 'Available Scripts')
+];
+
+foreach ($data['scripts'] as $script) {
+    $items[] = new CDiv(
+        sprintf(
+            '%s - %s (Scope:%s Type:%s)',
+            $script['scriptid'],
+            $script['name'],
+            $script['scope'],
+            $script['type']
+        )
+    );
+}
+
 (new CWidgetView($data))
-    ->addItem(
-        new CDiv('Hello World')
-    )
+    ->addItem($items)
     ->show();
