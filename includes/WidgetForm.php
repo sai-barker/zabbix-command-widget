d1b95a57277dda38ba66f3f04f79fc0105a880cf
sai-barker <sai.barker95@gmail.com>
2026-07-15 21:51:18 +1000

    Validate manual input against selected script

--- includes/WidgetForm.php
+++ includes/WidgetForm.php
@@ -20,6 +20,73 @@ use Zabbix\Widgets\Fields\{
 
 class WidgetForm extends CWidgetForm {
 
+	public function validate(bool $strict = false): array {
+		$errors = parent::validate($strict);
+
+		if ($errors) {
+			return $errors;
+		}
+
+		$scriptid = $this->getFieldValue('command_scriptid');
+		$manualinput = (string) $this->getFieldValue('command_manualinput');
+
+		$scripts = API::Script()->get([
+			'output' => [
+				'manualinput',
+				'manualinput_validator_type',
+				'manualinput_validator'
+			],
+			'scriptids' => [$scriptid]
+		]);
+
+		if (!$scripts) {
+			return [_('The selected script is unavailable or you do not have permission to use it.')];
+		}
+
+		$script = $scripts[0];
+
+		if ((int) $script['manualinput'] !== ZBX_SCRIPT_MANUALINPUT_ENABLED) {
+			return $errors;
+		}
+
+		$validator = $script['manualinput_validator'];
+
+		if ((int) $script['manualinput_validator_type'] === ZBX_SCRIPT_MANUALINPUT_TYPE_LIST) {
+			$allowed_values = array_map('trim', explode(',', $validator));
+
+			if (!in_array($manualinput, $allowed_values, true)) {
+				$errors[] = _s(
+					'Manual input must be one of: %1$s.',
+					implode(', ', $allowed_values)
+				);
+			}
+
+			return $errors;
+		}
+
+		$regex_validator = new \CRegexValidator([
+			'messageInvalid' => _('The input validation rule must be a string.'),
+			'messageRegex' => _('Incorrect regular expression "%1$s": "%2$s"')
+		]);
+
+		if (!$regex_validator->validate($validator)) {
+			$errors[] = $regex_validator->getError();
+
+			return $errors;
+		}
+
+		$regular_expression = '/'.str_replace('/', '\\/', $validator).'/';
+
+		if (!preg_match($regular_expression, trim($manualinput))) {
+			$errors[] = _s(
+				'Manual input does not match the script validation rule: %1$s',
+				$validator
+			);
+		}
+
+		return $errors;
+	}
+
 	public function addFields(): self {
 		$scripts = API::Script()->get([
 			'output' => ['scriptid', 'name'],
